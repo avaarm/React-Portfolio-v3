9374ec02b513bde11b942cea0b7588332ab2a469
Armenuhi <avaarm95@gmail.com>
2020-08-25 13:44:03 -0700

    Updates

---
+++ precache-manifest.064b7b5cd4d3c1d3710f5e488cc20e26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ba959acb529ce846e27",
+    "url": "/react-portfolio/static/css/main.d02c7a74.chunk.css"
+  },
+  {
+    "revision": "0ba959acb529ce846e27",
+    "url": "/react-portfolio/static/js/main.0ba959ac.chunk.js"
+  },
+  {
+    "revision": "6221052083d9d7c06dcf",
+    "url": "/react-portfolio/static/js/1.62210520.chunk.js"
+  },
+  {
+    "revision": "f8926043fa4459ec0101",
+    "url": "/react-portfolio/static/js/runtime~main.f8926043.js"
+  },
+  {
+    "revision": "e48f0db1abd6b3a1d77b2b83eda8c20a",
+    "url": "/react-portfolio/index.html"
+  }
+];
