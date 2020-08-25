0ccc369e9a10b5a5d55ef44c7eee5d2669cb4bcd
Armenuhi <avaarm95@gmail.com>
2020-08-25 13:45:16 -0700

    recreated a portfolio post deleting old version, so it can be deployed properly

---
+++ README.md
@@ -0,0 +1,96 @@
+# [React Portfolio]()
+
+
+[![GitHub License](https://img.shields.io/badge/License-orange.svg)](Develop/License/MIT.md)
+<!-- [![GitHub Test](https://img.shields.io/badge/Test-blue.svg)](#test) -->
+<!-- can put technologies here as shield icons and their versions -->
+
+
+## Description 
+
+This is my personal portfolio rebuilt with react. It contains a section about me, a personal portfolio and a resume.
+
+## Table of Contents
+
+* [Description](#Description)
+* [Technologies](#technologies)
+* [Application](#Application)
+* [Acknowledgements](#acknowledgements)
+* [Questions](#questions)
+
+## Technologies
+
+* JavaScript
+* Bootstrap
+* React
+* Node.js
+* npm
+* HTML
+* CSS
+
+
+## Application 
+
+![Finished Product](public/images/portfolio/app.jpg)
+
+<!-- ## Demonstration 
+
+![Application Demo](./public/assets/video.gif) -->
+
+
+<!-- ## Installation
+
+The following installations are needed for this package:
+1. npm init 
+2. npm i  
+3. npm install node
+    
+Once in the correct directory (server.js), run "node server.js" -->
+
+<!-- ## Instructions
+
+These are instructins & tips for anyone who wants to build this app or a similar app as practice:
+
+* Using MongoDB
+
+1. Make sure to start MongoDB in your terminal prior to running the application
+
+* Using Web app manifests
+
+Web app manifests are part of a collection of web technologies called progressive web apps (PWAs), which are websites that can be installed to a device’s homescreen without an app store. Unlike regular web apps with simple homescreen links or bookmarks, PWAs can be downloaded in advance and can work offline, as well as use regular Web APIs.
+The web app manifest provides information about a web application in a JSON text file, necessary for the web app to be downloaded and be presented to the user similarly to a native app.
+
+1. Create a manifest.webmanifest in a public folder and populate it with information about your images 
+2. Add a link to index.html (<link rel="manifest" href="manifest.webmanifest">)
+3. If you have seed data run the following, npm init > npm run seed > npm start
+
+* Using Service Worker
+
+A service worker (similar to AppCache) is a script that your broswer runs in the background, separate from a web page, and allows for a user to use their application offline. 
+
+1. Create a service-worker.js file in the public folder.
+2. Copy and analyze the code I have in my service-worker.js file
+
+
+* Using IndexedDB
+
+IndexedDB is a low-level API for client-side storage of significant amounts of structured data, including files/blobs. 
+
+1. Create a db.js file in the public folder
+2. Copy and analyze the code I have in my db.js file  -->
+
+
+## Acknowledgements
+
+* UW Coding Bootcamp instructional team and classmates
+    * test files already provided
+    * skeleton of html files already provided
+    * htmlRenderer.js already provided
+* [Bootstrap](https://getbootstrap.com/docs/4.1/getting-started/introduction/)
+
+
+## Questions 
+
+If you have any questions, please contact me on:
+GitHub at [avaarm](https://github.com/avaarm)
+Email at [Armenuhi](mailto:avaarm95@mail.com)
---
+++ src/resumeData.js
@@ -0,0 +1,112 @@
+let resumeData = {
+    "imagebaseurl":"https://avaarm.github.io/",
+    "name": "Armenuhi",
+    "role": "Full Stack Developer and Research Scientist",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/armenuhi-avanesyan-99101286/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/avaarm",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"twitter",
+          "url":"http://twitter.com/EmmaPrograms",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I love science and technology. I currently work in pre-Clinical research primarily on Hematopoetic Stem Cells and CAR T cells. I am working to start a non-profit that helps Armenian women who want to pursue careers in technology.",
+    "address":"avaarm@gmail.com ",
+    "website":"",
+    "education":[
+      {
+        "UniversityName":"University of Washington",
+        "specialization":"Full Stack Web Development Program",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2020",
+        "Achievements":"Through this program I was able to learn both front-end and back-end development"
+      },
+      {
+        "UniversityName":"University of California San Diego",
+        "specialization":"B.S. in Microbiology",
+        "MonthOfPassing":"June",
+        "YearOfPassing":"2016",
+        "Achievements":"William Stout Scholarship and LAUNCH Scholarship Recipient  "
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Fred Hutch Cancer Research Center",
+        "specialization":"Process Development Associate/ Research Scientist",
+        "MonthOfLeaving":"Present",
+        "YearOfLeaving":"",
+        "Achievements":"I am currently working on pre-clinical trial therapeutics for CAR-T cell companies in order to establish them for larger scale manufacturing."
+      },
+      {
+        "CompanyName":"Poseida Therapeutics, Inc.",
+        "specialization":"Reseach Technician in Gene Therapy and Immunology",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Created a high throughput chemical compound screening method and analysis for hematopoietic stem cells as well as training new group members on the use of cytometers, lab techniques, and general lab requirements. "
+      }
+    ],
+    "skillsDescription":"Here are a list of my pogramming related skills",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"API integration"
+      },
+      {
+        "skillname":"Javascript"
+      },
+      {
+        "skillname":"SQL & NoSQL"
+      },
+      {
+        "skillname":"Express"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Uplift",
+        "description":"A MySQL application that allows users to search for Black-Owned Businesses in their neighborhoods",
+        "imgurl":"images/portfolio/uplift.jpg",
+        "projecturl":"https://project-uplift.herokuapp.com/"
+      },
+      {
+        "name":"Better Earth",
+        "description":"A MERN application that allows the user to obtain sustainablity resources",
+        "imgurl":"images/portfolio/betterEarth.jpg",
+        "projecturl":"https://sheltered-sea-24671.herokuapp.com/"
+      },
+      {
+        "name":"Budget Tracker",
+        "description":"A full-stack application that allows the user to add and subtract funds onile or offline",  
+        "imgurl":"images/portfolio/budget.jpg",
+        "projecturl":"https://polar-ocean-72761.herokuapp.com/"
+      },
+      {
+        "name":"Workout Tracker",
+        "description":"A full-stack application that allows the user to add and complete workouts or exercises",
+        "imgurl":"images/portfolio/dashboard.png",
+        "projecturl":"https://limitless-hollows-96486.herokuapp.com/"
+      }
+    ]
+  }
+  
+  export default resumeData
